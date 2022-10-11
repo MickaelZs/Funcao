76169fa8b488d4774002ce9af744f3c63b458f14
Rodrigo Oliveira <rodrigo.anjos0806@gmail.com>
2022-10-11 17:42:23 -0300

    qaedwd

--- src/pages/abastecimento/index.js
+++ src/pages/abastecimento/index.js
@@ -1,7 +1,31 @@
+import { useState } from "react";
+
 export default function Index(){
+
+    const [capac, setCapac] = useState(0.0);
+    const [consumo, setConsumo] = useState(0.0);
+    const [dist, setDist] = useState(0.0);
+    const [resposta, setResposta] = useState( );
+
+    function CalcularParadas() {
+        try {
+            let litros = dist/consumo;
+            let paradas = litros/capac;
+
+            setResposta((Math.ceil(paradas)).toFixed(1));
+        } catch (err) {
+            setResposta(err.message);
+        }
+    }
+
     return(
         <div>
-            
+            <h1>Abastecimento</h1>
+            <p>Insira a capacidade do tanque: <input type="number" value={capac} onChange={e => setCapac(e.target.value)} /> </p>
+            <p>Insira o consumo do carro: <input type="number" value={consumo} onChange={e => setConsumo(e.target.value)} /> </p>
+            <p>Insira a distância a ser percorrida: <input type="number" value={dist} onChange={e => setDist(e.target.value)} /> </p>
+            <button onClick={CalcularParadas}>Calcular</button>
+            <h2>{resposta}</h2>
         </div>
     )
 }
--- src/pages/salario/index.js
+++ src/pages/salario/index.js
@@ -1,7 +1,30 @@
+import { useState } from "react"
+
 export default function Index(){
+
+    const [salario, setSalario] = useState(0)
+    const [bonus, setBonus] = useState(0)
+    const [desc, setDesc] = useState(0)
+    const [resposta, setResposta] = useState()
+
+    function Liquido () {
+        try {
+            let b = salario*bonus/100;
+            let s = salario - desc + b;
+            setResposta(s); 
+        } catch (err) {
+            setResposta(err.message)
+        }
+    }
+
     return(
         <div>
-            
+            <h1>Salário</h1>
+            <p>Insira o salário <input type="number" value={salario} onChange={e => setSalario(e.target.value)} /> </p>
+            <p>Insira o bônus <input type="number" value={bonus} onChange={e => setBonus(e.target.value)} /> </p>
+            <p>Insira o desconto <input type="number" value={desc} onChange={e => setDesc(e.target.value)} /> </p>
+            <button onClick={Liquido}>Calcular</button>
+            <h2>{resposta}</h2>
         </div>
     )
 }
