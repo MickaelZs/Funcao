d3ca3b243d522c21e3d925646e8ab50204d3837b
MickaelZs <mickaelcosta431@gmail.com>
2022-10-11 16:45:17 -0300

    .

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import Acai from './pages/açai/index.js' 
 import Signo from './pages/signo/index.js'
 import Sorveteria from './pages/sorveteria/index.js'
+import Febre from './pages/febre'
+import Salario from './pages/salario'
+import Abastecimento from './pages/abastecimento'
 import { BrowserRouter,Route, Routes } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -14,7 +17,10 @@ root.render(
         <Route path='/' element={<Acai />} />
         <Route path='/signo' element={<Signo />} />
         <Route path='/sorveteria' element={<Sorveteria />} />
-      </Routes>
+        <Route path='/febre' element={<Febre/>} />
+        <Route path='/abastecimento' element={<Abastecimento />} />
+        <Route path='/salario' element={<Salario />} />     
+         </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
