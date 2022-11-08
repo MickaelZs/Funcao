fbc0a70b51badd6c0c9f6733f67ae24766145c53
Rodrigo Oliveira <rodrigo.anjos0806@gmail.com>
2022-11-08 17:33:33 -0300

    Simplesmente Rodrigo Juegos

--- teste.js
+++ teste.js
@@ -1,14 +1,31 @@
-function Alunos (num) {
-    try {
+const readline = require('prompt-sync')();
 
-        let x = [];
-        for (let i = 1; i <= num; i++) {
-            x = [...x, i];
+function sorteio() {
+    let sorteado = parseInt(Math.random() * 1000)
+    let numUsuario = 0;
+
+    console.log("ADIVINHE O NÚMERO")
+    for(let tentativa = 1; tentativa <= 10; tentativa++) {
+        console.log(`Tentativa ${tentativa}`);
+        numUsuario = Number(readline());
+        if (isNaN(numUsuario)) {
+            throw new Error("ENFIA ESSA LETRA NO CU DA SUA MÃE")
         }
-        return x;
-    } catch (err) {
-        return err.message;
+        if (numUsuario == sorteado) {
+            tentativa = 30;
+            console.log("ACERTO MISERAVI")
+        }
+        if (numUsuario < sorteado) {
+            console.log("Maior")          
+        }
+        if (numUsuario > sorteado) {
+            console.log("Menor")          
+        }
+        if (tentativa == 10 && numUsuario !== sorteado) {
+            console.log(`perdeu!!! a resposta certa era ${sorteado}`)
+        }
     }
+    
 }
 
-console.log(Alunos(5));
+sorteio()
